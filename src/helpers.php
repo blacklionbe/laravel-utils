51aaa9fcc43778402020602e06a73a68485ce8c3
Klaas Geldof <klaas@blacklion.be>
2019-09-11 21:32:57 +0200

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -25,26 +25,3 @@ if (! function_exists('ie')) {
         return preg_match('~MSIE|Internet Explorer~i', $agent) || (strpos($agent, 'Trident/7.0') !== false && strpos($agent, 'rv:11.0') !== false);
     }
 }
-
-if (! function_exists('view_to_pdf')) {
-    function view_to_pdf($view = null, $data = [])
-    {
-        $snappy = app('snappy.pdf');
-        $snappy->setOption('encoding', 'UTF-8');
-        $snappy->setOption('page-size', 'A4');
-
-        $html = view($view, $data)->render();
-
-        return $snappy->getOutputFromHtml($html);
-    }
-}
-
-if (! function_exists('view_to_pdf_url')) {
-    function view_to_pdf_url($view = null, $data = [])
-    {
-        $filename = 'downloads/'.str_random(40);
-        Storage::put($filename, view_to_pdf($view, $data));
-
-        return Storage::url($filename);
-    }
-}
